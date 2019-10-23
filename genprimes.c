668744e8c39caad0f5740d7ca9b9e667fae40014
markflaherty <47069579+markflaherty@users.noreply.github.com>
2019-10-23 19:30:37 -0400

    miss

--- genprimes.c
+++ genprimes.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-int prime(int x){
+#include "mpi.h"
+int prime(int start, int end){
+	int x = end;
 	int i,j,k,m;
-	printf("%s\n", "hello"  );
-	int lim = (x+1)/2;
+	int lim = (int)(x+1)/2;
 	int count = 0;
 	int a[x+1];
-	for(j = 2; j <= x; j++){
+	for(j = start; j <= end; j++){
 		a[j] = j;
 	}
-	for(i = 2; i <= lim; i++){
+	for(i = start; i <= lim; i++){
 		int curr = a[i];
 		if(curr == 0){
 			continue;
 		}
-		for(k = i; k <= x; k++){
+		for(k = i; k <= end; k++){
 			if(a[k] != 0){
 				if(a[k] != curr && a[k]%curr == 0){
 					a[k] = 0;
@@ -23,7 +24,7 @@ int prime(int x){
 			}
 		}
 	}
-	for(m = 0; m <= x; m++){
+	for(m = start; m <= end; m++){
 		if(a[m] != 0){
 			count++;
 		}
@@ -31,17 +32,24 @@ int prime(int x){
 	return count;
 }
 int main(){
-	int j;
 	int limit;
+	int tasks;
+	int rank;
+	int myStart;
+	int myEnd;
+	int sum = 0;
+	MPI_Init(&argc,&argv);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	MPI_Comm_size(MPI_COMM_WORLD,&tasks);
 	printf("Enter a bound: ");
 	scanf("%d",&limit);
-	if(limit == 10){
-		printf("%d\n", 4);
-	}
-	else{
-		int count = prime(limit);
-		printf("%d\n", count);
-		printf("say my name say my name, \nif no one is around you say baby I love you, \nif you ain't running games\n");
-	}
+	int jump = (int)(limit/tasks);
+	myStart = rank*jump;
+	myEnd = myStart+jump+1;
+	int count = prime(myStart, myEnd);
+	MPI_Reduce(&count, &sum, 1, MPI_SUM,0,MPI_COMM_WORLD);
+	printf("%d\n", sum);
+	MPI_Finalize();
+		
 
 }
