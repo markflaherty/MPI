630744af0062cd138563d13aab2dd2b5b8f34e81
markflaherty <47069579+markflaherty@users.noreply.github.com>
2019-10-23 23:39:18 -0400

    weaksauce

--- genprimes.c
+++ genprimes.c
@@ -45,9 +45,10 @@ int main(int argc, char *argv[]){
 	int jump = (int)(limit/tasks);
 	myStart = rank*jump;
 	myEnd = myStart+jump+1;
-	int count = prime(myStart, myEnd);
-	MPI_Reduce(&count, &sum, 1, MPI_INT, MPI_SUM,0,MPI_COMM_WORLD);
-	printf("%d\n", sum);
+	printf("%d\t %d\t %d\t %d\n", rank, myStart, jump, myEnd );
+	//int count = prime(myStart, myEnd);
+	//MPI_Reduce(&count, &sum, 1, MPI_INT, MPI_SUM,0,MPI_COMM_WORLD);
+	//printf("%d\n", sum);
 	MPI_Finalize();
 		
 
