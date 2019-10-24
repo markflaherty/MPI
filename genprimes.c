29f0c781d866a5fa0172bf414431d71ea077060c
markflaherty <47069579+markflaherty@users.noreply.github.com>
2019-10-23 20:23:38 -0400

    hello

--- genprimes.c
+++ genprimes.c
@@ -44,11 +44,12 @@ int main(int argc, char *argv[]){
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	MPI_Comm_size(MPI_COMM_WORLD,&tasks);
 	int jump = (int)(limit/tasks);
+	printf("%d\t %d\n",rank, tasks);
 	myStart = rank*jump;
 	myEnd = myStart+jump+1;
-	int count = prime(myStart, myEnd);
-	MPI_Reduce(&count, &sum, 1, MPI_INT, MPI_SUM,0,MPI_COMM_WORLD);
-	printf("%d\n", sum);
+	//int count = prime(myStart, myEnd);
+	//MPI_Reduce(&count, &sum, 1, MPI_INT, MPI_SUM,0,MPI_COMM_WORLD);
+	//printf("%d\n", sum);
 	MPI_Finalize();
 		
 
