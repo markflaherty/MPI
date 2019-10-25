f8d6f3ec75086f76bd16af2c8275c8809753529c
markflaherty <47069579+markflaherty@users.noreply.github.com>
2019-10-25 11:43:59 -0400

    hello

--- genprimes.c
+++ genprimes.c
@@ -8,7 +8,6 @@ int prime(int start, int end, int rank){
 	int count = 0;
 	int distance = end-start;
 	int *a = malloc(sizeof(int)*distance);
-	printf("%d\t %d\t %s %d\n", rank, start, "yes", end);
 	for(j = start; j < end-1; j++){
 		a[j] = j;
 	}
@@ -31,6 +30,7 @@ int prime(int start, int end, int rank){
 		}
 	}
 	free(*a);
+	printf("%d\t %d\t %s\t %d\t %d\n", rank, start, "yes",count,end);
 	return count;
 }
 int limit = 100;
