723f925a53f72de3849aa95f5252118ac74fc4b5
markflaherty <47069579+markflaherty@users.noreply.github.com>
2019-10-25 01:13:13 -0400

    yu

--- genprimes.c
+++ genprimes.c
@@ -70,6 +70,7 @@ int main(int argc, char *argv[]){
 	}
 	*/
 	//int count = prime(myStart, myEnd);
+	int count = 0;
 	printf(" %d\t %d\t %d\t %d\t %d\t %d\n",count,tasks,jump, rank, myStart, myEnd);
 	//MPI_Reduce(&count, &sum, 1, MPI_INT, MPI_SUM,0,MPI_COMM_WORLD);
 	/*
