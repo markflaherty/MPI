7e036c2b7fd08f6b343fcd9bb8550fe003286e48
markflaherty <47069579+markflaherty@users.noreply.github.com>
2019-10-26 19:34:40 -0400

    here

--- genprimes.c
+++ genprimes.c
@@ -9,69 +9,43 @@ int main (int argc, char ** argv) {
   int size;
   int prime;
   int count;
-  int global_count;
+  int g;
   int first;
-  long int high_value;
-  long int low_value;
-  int comm_rank;
+  long int high;
+  long int low;
+  int rank;
   int comm_size;
-  char * marked;
-  double runtime;
-  
+  char * hit;
+  double t;
   MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &comm_rank);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
-  
   MPI_Barrier(MPI_COMM_WORLD);
-  runtime = -MPI_Wtime();
-  /*
-  // Check for the command line argument.
-  if (argc != 2) {
-    if (comm_rank == 0) printf("Please supply a range.\n");
-    MPI_Finalize();
-    exit(1);
-  }
-  */
-  //n = atoi(argv[1]);
-  
-  // Bail out if all the primes used for sieving are not all held by
-  // process zero.
-  /*
-  if ((2 + (n - 1 / comm_size)) < (int) sqrt((double) n)) {
-    if (comm_rank == 0) printf("Too many processes.\n");
-    MPI_Finalize();
-    exit(1);
-  }
-  */
-  // Figure out this process's share of the array, as well as the integers
-  // represented by the first and last array elements.
-  low_value  = 2 + (long int)(comm_rank) * (long int)(n - 1) / (long int)comm_size;
-  high_value = 1 + (long int)(comm_rank + 1) * (long int)(n - 1) / (long int)comm_size;
-  size = high_value - low_value + 1;
-  
-  marked = (char *) calloc(size, sizeof(char));
-  
-  if (marked == NULL) {
-   printf("Cannot allocate enough memory.\n");
+  t = -MPI_Wtime();
+  low  = 2 + (long int)(comm_rank) * (long int)(n - 1) / (long int)comm_size;
+  high = 1 + (long int)(comm_rank + 1) * (long int)(n - 1) / (long int)comm_size;
+  size = high - low + 1;
+  hit = (char *) calloc(size, sizeof(char));
+  if (hit == NULL) {
    MPI_Finalize();
    exit(1);
   }
   
-  if (comm_rank == 0) index = 0;
+  if (rank == 0) index = 0;
   prime = 2;
   
   do {
-    if (prime * prime > low_value) {
-      first = prime * prime - low_value;
+    if (prime * prime > low) {
+      first = prime * prime - low;
     } else {
-      if ((low_value % prime) == 0) first = 0;
-      else first = prime - (low_value % prime);
+      if ((low % prime) == 0) first = 0;
+      else first = prime - (low % prime);
     }
     
-    for (i = first; i < size; i += prime) marked[i] = 1;
+    for (i = first; i < size; i += prime) hit[i] = 1;
     
-    if (comm_rank == 0) {
-      while (marked[++index]);
+    if (rank == 0) {
+      while (hit[++index]);
       prime = index + 2;
     }
     
@@ -80,19 +54,19 @@ int main (int argc, char ** argv) {
   
   count = 0;
   
-  for (i = 0; i < size; i++) if (marked[i] == 0) count++;
+  for (i = 0; i < size; i++) if (hit[i] == 0) count++;
   
   if (comm_size > 1) {
-    MPI_Reduce(&count, &global_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&count, &g, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
   } else {
-    global_count = count;
+    g = count;
   }
   
-  runtime += MPI_Wtime();
+  t += MPI_Wtime();
   
-  if (comm_rank == 0) {
+  if (rank == 0) {
     printf("In %f seconds we found %d primes less than or equal to %d.\n",
-		runtime, global_count, n);
+		t, g, n);
   }
   
   MPI_Finalize();
