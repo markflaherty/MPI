48d75cf3717cbf22e7e72a714ae024478dbee08b
markflaherty <47069579+markflaherty@users.noreply.github.com>
2019-10-26 21:18:25 -0400

    hizzle

--- genprimes.sh
+++ genprimes.sh
@@ -2,11 +2,12 @@
 
 set -e 
 
-numP = $1
+numP = "2"
+numN = $1
 numN = "100"
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	module load mpi/openmpi-x86_64
 mpicc -g -Wall -o genprimes genprimes.c -std=c99
-mpirun -n $numP ./genprimes $numN > outputs/$numN
+mpirun -n $numP ./genprimes $1 > outputs/$1
 
 cat outputs/$numN
